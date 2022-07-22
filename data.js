0ae924a9fe8b6ad2e6102be873db625f89c4228d
Sergey <sergastrah@yandex.ru>
2022-07-22 17:41:25 +0400

    create AudioPlayer

---
+++ data.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: './assets/music/0581b0f11cd3e54334.mp3',
+        artist: 'artist 1',
+        cover: './assets/image/Microfon1.png'
+    },
+    {
+        name: 'song 2',
+        path: './assets/music/33459.mp3',
+        artist: 'artist 2',
+        cover: './assets/image/Microfon2.jpg'
+    },
+    {
+        name: 'song 3',
+        path: './assets/music/september_day_01_stereo.mp3',
+        artist: 'artist 3',
+        cover: './assets/image/Microfon3.jpg'
+    },
+    {
+        name: 'song 4',
+        path: './assets/music/sunrise_105.mp3',
+        artist: 'artist 4',
+        cover: './assets/image/plastinki.jpg'
+    },
+] 
